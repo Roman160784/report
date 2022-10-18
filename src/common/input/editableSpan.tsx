53a91fbdc08951de67f8d222940556f2b3b07b3b
Roman160784 <matveyenkaraman@gmail.com>
2022-10-18 18:56:39 +0300

    add editableSpan component

---
+++ src/common/input/editableSpan.tsx
@@ -0,0 +1,43 @@
+import React, { ChangeEvent, useState, KeyboardEvent } from 'react';
+import classes from './editableSpan.module.css'
+
+type EditableSpanPropsType = {
+    title: string
+    changeTitle: (title: string) => void
+}
+
+export const EditableSpan = ({ title, changeTitle, ...props }: EditableSpanPropsType) => {
+
+   
+
+    const [value, setValue] = useState<string>(title)
+    const [mode, setMode] = useState<boolean>(false)
+    const [error, setError] = useState<string | null>(null)
+
+    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setValue(e.currentTarget.value)
+        setError(null)
+       
+    }
+
+    const onBlurHandler = () => {
+         if (value.trim() !== '') {
+            changeTitle(value)
+            setValue('')
+            setMode(false)
+        }
+    }
+
+    return (
+        <div>
+            {mode
+                ? <input type="text" value={value} onChange={onChangeHandler}
+                         onBlur={onBlurHandler} autoFocus  className={classes.inputName}/>
+                : <span onDoubleClick={() => {setMode(true)}}>{title}</span>}
+            {error && <div className={classes.error}>{error}</div>}
+            <div className={classes.inputClick}>Кликни дважды для изменения названия</div>
+        </div>
+
+    )
+
+}
