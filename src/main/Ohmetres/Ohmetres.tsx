715a3d4dde5c97f2e6c410b6e473cb6402b3754d
Roman160784 <matveyenkaraman@gmail.com>
2022-12-14 15:24:43 +0300

    finished Ohmetr report

--- src/main/Ohmetres/Ohmetres.tsx
+++ src/main/Ohmetres/Ohmetres.tsx
@@ -1,13 +1,132 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { AmperOhmetresHeaderStandards } from '../../common/amperOhmmetres/amperOhmmetres';
+import { DataAmperOhmForReport } from '../../common/dataAmperOhmForReport/DataAmperOhmForReport';
+import { DataHeader } from '../../common/dataHeader';
+import { AmprOhmStandardsInput } from '../../common/input/AmperOhmStandardsInput';
+import { EditableSpan } from '../../common/input/editableSpan';
 import { NameOrganization } from '../../common/nameOrganization';
+import { Standards } from '../../common/standarts/standards';
+import { HeaderReducerType, setReportNumberAC, setUserAC } from '../../redux/HeaderReducer';
+import { addOhmetrAC, addStandardForOhmetrAC, chengeOhmetresTitleAC, chengeStandardOhmetrTitleAC, chengeStigmaNumberOhmetrAC, ohmmetresReducerType, removeOhmetrAC, removeStandardOhmetresAC } from '../../redux/OhmmetresReducer';
+import { RootReducerType } from '../../redux/store';
+import style from './ohmetr.module.css';
 
 export const Ohmetres = () => {
+    
+    const headerData = useSelector<RootReducerType, HeaderReducerType>(state => state.header)
+    const ohmetres = useSelector<RootReducerType, ohmmetresReducerType[]>(s => s.ohmetres)
+    const standardsOhmetres = ohmetres.filter(el => el.standardsOhmmetres)
+    const dataOhmetres = ohmetres.filter(el => el.ohmmetr)
+    const dispatch = useDispatch()
+
+    const reportNumberHandler = (reportNumber: string) => {
+        dispatch(setReportNumberAC(reportNumber));
+    }
+
+    const chengeStandardName = (id: string, title: string, key: string) => { 
+        dispatch(chengeStandardOhmetrTitleAC(id, title, key))
+    }
+
+    const removeStandard = (id: string) => {
+        dispatch(removeStandardOhmetresAC(id))
+    }
+    const addStandard = (title: string) => {
+        dispatch(addStandardForOhmetrAC(title))
+    }
+
+    const chengeOhmetresParamethr = (id: string, title: string, key: string) => {
+        dispatch(chengeOhmetresTitleAC(id, title, key))
+    }
+    const chengeOhmetresStigmaNumber = (id: string, num: number) => {
+        dispatch(chengeStigmaNumberOhmetrAC(id, num))
+    }
+    const removeOhmetr = (id: string) => {
+        dispatch(removeOhmetrAC(id))
+    }
+    const addOhmetr = (title: string) => {
+        dispatch(addOhmetrAC(title))
+    }
+    const userNameHandler = (userName: string) => {
+        dispatch(setUserAC(userName));
+    }
 
     return(
         <div>
             <NameOrganization/>
-        <div>Омметры</div>
+        
+        <div className={style.reportNum}>
+                Протокол № <EditableSpan title={headerData.reportNumber} changeTitle={reportNumberHandler} /> /01/2160
+        </div>
+        <div>
+                <DataHeader />
+            </div>
+            <div>
+                ТНПА: ГОСТ 8.409-81
+            </div>
+            <br />
+            <div>Средства поверки: <AmprOhmStandardsInput setDataForAmperOhm={addStandard}/> <span>введите заводской № СИ</span></div> 
+            <br />
+            <div>
+            <table border={1} >
+                    <AmperOhmetresHeaderStandards />
+
+                    {
+                        standardsOhmetres.map(st => {
+                            return (
+                                <Standards standard={st.standardsOhmmetres} 
+                                chengeStandardName={chengeStandardName}
+                                removeStandard={removeStandard}
+                                />
+                            )
+                        })
+                    }
+
+                </table>
+            </div>
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <div><AmprOhmStandardsInput setDataForAmperOhm={addOhmetr}/> <span>введите заводской № СИ</span></div>
+            <br />
+            <div>
+            <table border={1} >
+                    <tr>
+                        <th>Номер<br /> п.п</th>
+                        <th>Тип,<br /> предел измерений</th>
+                        <th>Заводской<br /> номер</th>
+                        <th>Класс<br /> точночти</th>
+                        <th>Внешний осмотр, <br />согласно п. 4.1</th>
+                        <th>Опробование,<br /> согласно п. 4.4</th>
+                        <th>Определение основной погрешности п. 4.9, <br /> вариации показаний п. 4.9</th>
+                        <th>Заключение</th>
+                        <th>Номер <br /> клейма-наклейки</th>
+                    </tr>
+                    {
+                        dataOhmetres.map(data => {
+                            return (
+                                <DataAmperOhmForReport ampermetr={data.ohmmetr}
+                                chengeAmpermetresTitle={chengeOhmetresParamethr}
+                                chengeStigmaNumber={chengeOhmetresStigmaNumber}
+                                removeAmpermetr={removeOhmetr}
+                                />
+                            )
+                        })
+                    }
+                </table>
+            </div>
+            <br />
+            <div>
+                Государственный поверитель ______________________ <EditableSpan title={headerData.user} changeTitle={userNameHandler} />
+            </div>
         </div>
     )
     
 }
+
+
--- src/redux/OhmmetresReducer.ts
+++ src/redux/OhmmetresReducer.ts
@@ -1,3 +1,4 @@
+import { title } from 'process';
 import { v1 } from 'uuid';
 //types
 
@@ -31,7 +32,7 @@ export type ohmmetresReducerType = {
 const initialStateOhmmetres: ohmmetresReducerType[] = [{
     ohmmetr:  [{id: v1(),
         count: 1,
-        type: 'ЭС0202/2-г',
+        type: 'ЭС0202/2-Г',
         number: '1111',
         class: '15',
         visual: 'Соответствует',
@@ -76,10 +77,66 @@ const initialStateOhmmetres: ohmmetresReducerType[] = [{
 }]
 
 export const OhmmetresReducer = (state: ohmmetresReducerType[] = initialStateOhmmetres, action: MainActionType):ohmmetresReducerType[] => {
-    switch (action){
+    switch (action.type){
+        case 'OHMETR/CHENGE-STANDARD-PARAMETHR' : {
+            return state.map(st => 
+                ({...st, standardsOhmmetres: st.standardsOhmmetres.map(s => s.id === action.id 
+                    ? {...s, [action.key] : action.title} : s )}))
+        }
+        case 'OHMETR/REMOVE-STANDARD': {
+            return state.map(st => ({...st, standardsOhmmetres: st.standardsOhmmetres.filter(s => s.id !== action.id)}))
+        }
+        case 'OHMETR/ADD-STANDARD' : {
+            const arrayStandardsOhmetres = state[0].standardsOhmmetres
+            const lastOhmetrStandard = arrayStandardsOhmetres[arrayStandardsOhmetres.length - 1]
+            const newStandardOhmetr = {...lastOhmetrStandard, id: v1(), number: action.title}
+            const copyState = [...state]
+            copyState[0].standardsOhmmetres.unshift(newStandardOhmetr)
+            return copyState
+        }
+        case 'OHMETR/CHENGE-OHMETR-PARAMETHR' : {
+            return state.map(st => ({...st, ohmmetr: st.ohmmetr.map(o => o.id === action.id ? {...o, [action.key] : action.title} : o)}))
+        }
+        case 'OHMETR/CHENGE-STIGMA-NUMBER' : {
+            return state.map(st => ({...st, ohmmetr: st.ohmmetr.map(o => o.id === action.id ? {...o, stigma : action.num} : o)}))
+        }
+        case 'OHMETR/REMOVE-OHMETR' : {
+            return state.map(st => ({...st, ohmmetr: st.ohmmetr.filter(o => o.id !== action.id)}))
+        }
+        case 'OHMETR/ADD-OHMETR' : {
+            const arrayOhmetres = state[0].ohmmetr
+            const lastOhmetr = arrayOhmetres[arrayOhmetres.length - 1]
+            const newOhmetr = {...lastOhmetr, id: v1(), number: action.title, stigma : lastOhmetr.stigma + 1}
+            const copyState = [...state]
+            copyState[0].ohmmetr.push(newOhmetr)
+            return copyState
+        }
         default:
             return state
     }
 } 
 
-export type MainActionType = ''
+export type MainActionType = chengeStandardOhmetrTitleACType | removeStandardOhmetresACType | chengeOhmetresTitleACType
+| chengeStigmaNumberOhmetrACType | removeOhmetrACType | addStandardForOhmetrACType | addOhmetrACType
+
+
+export type chengeStandardOhmetrTitleACType = ReturnType<typeof chengeStandardOhmetrTitleAC>
+export type removeStandardOhmetresACType = ReturnType<typeof removeStandardOhmetresAC>
+export type addStandardForOhmetrACType = ReturnType<typeof addStandardForOhmetrAC>
+export type chengeOhmetresTitleACType = ReturnType<typeof chengeOhmetresTitleAC>
+export type chengeStigmaNumberOhmetrACType = ReturnType<typeof chengeStigmaNumberOhmetrAC>
+export type removeOhmetrACType = ReturnType<typeof removeOhmetrAC>
+export type addOhmetrACType = ReturnType<typeof addOhmetrAC>
+
+export const chengeStandardOhmetrTitleAC = (id: string, title: string, key: string) => (
+    { type: 'OHMETR/CHENGE-STANDARD-PARAMETHR', id, title, key } as const)
+
+export const removeStandardOhmetresAC = (id: string,) => ({ type: 'OHMETR/REMOVE-STANDARD', id } as const)
+export const addStandardForOhmetrAC = (title: string) => ({ type: 'OHMETR/ADD-STANDARD', title } as const)
+
+export const chengeOhmetresTitleAC = (id: string, title: string, key: string) => (
+    { type: 'OHMETR/CHENGE-OHMETR-PARAMETHR', id, title, key } as const) 
+    
+export const chengeStigmaNumberOhmetrAC = (id: string, num: number) => ({type: 'OHMETR/CHENGE-STIGMA-NUMBER', id, num} as const)  
+export const removeOhmetrAC = (id: string,) => ({ type: 'OHMETR/REMOVE-OHMETR', id } as const)  
+export const addOhmetrAC = (title: string,) => ({ type: 'OHMETR/ADD-OHMETR', title } as const)  
